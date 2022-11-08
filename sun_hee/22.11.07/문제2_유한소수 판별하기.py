173287d3aa68f5633525cec1d655220b52a2d1ec
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2022-11-08 14:00:49 +0900

    11/8 김선희 문제풀이

--- sun_hee/22.11.07/문제2_유한소수 판별하기.py
+++ sun_hee/22.11.07/문제2_유한소수 판별하기.py
@@ -9,5 +9,22 @@ def solution(a, b):
             y = y - x
     z = b/(x + y)
 
+    list = []
+    for i in range(1, int(z+1)):
+        list.append(z/i)
+        if len(list) >= 3:
+            return 2
+        if len(list) == 2:
+            for i in range(list):
+                if i == 2 and i == 5:
+                    return 1
+                else:
+                    return 2
+        if len(list) == 1:
+            if i == 2 or 5:
+                return 1
+            else:
+                return 2
+
 
 print(solution(11, 22))
