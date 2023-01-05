a479c5f6eecb0eba63b62e4e88479aaba0e9e4ab
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2023-01-06 00:57:48 +0900

    1/6 김선희 문제풀이(이코테)

---
+++ sun_hee/python 개념 연습/23.1.5 dictionary.py
@@ -0,0 +1,68 @@
+import operator
+#################리스트 만들기#################
+
+# (keys)
+a = {'name': 'pey', 'phone': '010-9999-1234', 'birth': '1118'}
+
+a.keys()
+list(a.keys())
+# dict_keys(['name', 'phone', 'birth'])
+# dict_keys 객체는 리스트를 사용하는 것과 차이가 없지만 리스트 고유의 append, insert, pop, remove, sort함수는 수행할 수 없다.
+
+# (values)
+
+a.values()
+# dict_values(['pey', '010-9999-1234', '1118'])
+
+# Key, Value 쌍 얻기(items)
+
+a.items()
+# dict_items([('name', 'pey'), ('phone', '010-9999-1234'), ('birth', '1118')])
+# x튜플로 묶은 리스트를 dict_items 객체로 돌려줌
+
+a.clear()
+a
+# {}
+# 모든 요소 삭제
+
+#################딕셔너리 key 정렬(sorted 이용)##############
+
+# import operator가 필요
+test = {"banana": 3, "apple": -3, "carrot": 1}
+
+# 오리지널 딕셔너리
+print(f'origin       : {test}\n')
+
+# dictionary.items()
+test1 = sorted(test.items())
+print(test1)
+# [('apple', -3), ('banana', 3), ('carrot', 1)] 오름차순으로 반환해 주지만 이렇게 리스트를 반환한다.
+print(f'sorted(test.item())       : {test1}')
+# 다시 딕셔너리로
+print(f'dict(sorted(test.item()       : {dict(test1)}')
+
+# 키 값만을 빼서 정렬하기
+test2 = sorted(test)
+print(f'sorted(test)       : {test2}')
+
+# 내림차순 정렬
+test3 = sorted(test.items(), reverse=True)
+print(test3)
+
+#################딕셔너리 value 정렬##############
+# 방법1: operator.itemgetter
+# 방법2: lambda(최신)
+
+# 딕셔너리에서 value값에 접근해서 sorted 함수의 정렬 키값으로 넘기기 위해 사용
+
+test = {"banana": 3, "apple": -3, "carrot": 1}
+
+# 방법1
+
+test4 = sorted(test.items(), key=operator.itemgetter(1))
+print(test4)
+
+# 방법2
+
+test5 = sorted(test.items(), key=lambda x: x[1])
+print(test5)
--- sun_hee/이코테/회의실문제.py
+++ sun_hee/이코테/회의실문제.py
@@ -4,7 +4,26 @@
 # 입력 : 이용자의 수 n과 순서대로 이용자의 시작시간과 끝나는 시간을 입력한다.
 # 출력 : 이용할 수 있는 최대 인원 수를 출력한다.
 
-n = int(input())
+# 풀이
+# 끝나는 시간 기준으로 정렬
+# 시작시간 순서로 확인하기. 겹치면 빼기
 
+n = int(input())
+cnt = 1
+time = {}
 for i in range(n):
-    print(list(map(int, input().split())))
+
+    a, b = map(int, input().split())
+    # 끝나는 시간을 key값으로 넣기
+    time[b] = a
+    # [(),()] 형태로 정렬됨
+time = sorted(time.items())
+
+
+for i in range(len(time)):
+    if time[i][1] >= time[i+1][1]:
+        cnt += 1
+
+print(cnt)
+
+# 위 코드는 틀린거임!! 문제점 : 시작 시간을 먼저 정렬하고 끝나는 시간을 정렬 해야됨!!
