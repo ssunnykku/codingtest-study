394c8caf2138ba9bee3aceb48ac38dbab8e13def
[AI5/2반] 김선희 <ssunnykku@gmail.com>
2023-02-06 14:13:45 +0900

    2/6 이코테 그리디, DFS/BFS

---
+++ sun_hee/이코테/5. DFS,BFS/5-9 BFS 예제.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+# BFS 함수 정의
+
+
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
