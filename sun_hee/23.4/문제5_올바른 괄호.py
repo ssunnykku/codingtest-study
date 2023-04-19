bdd41b6e8edb205cc463a09181022b649419de06
ssunnykku <ssunnykku@gmail.com>
2023-04-19 18:13:11 +0900

    4/19 문제풀이

--- sun_hee/23.4/문제5_올바른 괄호.py
+++ sun_hee/23.4/문제5_올바른 괄호.py
@@ -1,7 +1,23 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12909
 
-s = '(())()'
+s = ')'
 
+# 주어진 s를 순회
+def solution(s):
+    stack = []
 
+    for i in s:
+        #     왼쪽 괄호면 stack에 넣어줌
+        if i == '(':
+            stack.append(i)
+        # 스택이 비었는데 ")" 만나면?
+        elif i == ")" and stack == []:
+            return False
+        elif i == ")":
+            stack.pop()
+    if stack == []:
+        return True
+    else:
+        return False
 
-        # 풀이 실패..
+print(solution(s))
