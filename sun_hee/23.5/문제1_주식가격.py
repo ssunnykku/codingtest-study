e6d24736d7d811f07d551fd41e46fab5e8cf7991
ssunnykku <ssunnykku@gmail.com>
2023-05-08 18:28:16 +0900

    5/8 문제풀이

---
+++ sun_hee/23.5/문제1_주식가격.py
@@ -0,0 +1,35 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3
+
+# 가격이 떨어지지 않은 기간이 몇 초인지 리턴
+from collections import deque
+
+prices = [1, 2, 3, 2, 3]
+
+
+# 현재 숫자 이후 탐색
+# 만약 현재 숫자보다 크다면 +1?
+
+
+def solution(prices):
+    d = deque(prices)
+    answer = []
+    for i in range(len(prices)):
+        cnt = 0
+        for j in list(d)[1:]:
+            if j >= list(d)[0]:
+                cnt += 1
+            else:
+                break
+        if cnt == 0:
+            answer.append(1)
+        else:
+            answer.append(cnt)
+
+        d.popleft()
+    answer[-1] = 0
+    return answer
+
+
+print(solution(prices))
+
+# 실패!!!!!!
