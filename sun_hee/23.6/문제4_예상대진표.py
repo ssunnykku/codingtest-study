15f20d5701420d7fbe9115db217475161bc5af7b
ssunnykku <ssunnykku@gmail.com>
2023-06-13 20:33:17 +0900

    6/13 문제풀이

--- sun_hee/23.6/문제4_예상대진표.py
+++ sun_hee/23.6/문제4_예상대진표.py
@@ -1,1 +1,22 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12985
+
+# N명이 참가
+n = 8
+a = 4
+b = 7
+
+import math
+
+
+def solution(n, a, b):
+    answer = 1
+
+    while math.ceil(a / 2) != math.ceil(b / 2):
+        a = math.ceil(a / 2)
+        b = math.ceil(b / 2)
+        answer += 1
+
+    return answer
+
+
+print(solution(n, a, b))
